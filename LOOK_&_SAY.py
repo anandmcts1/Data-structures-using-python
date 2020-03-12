720fc2b42c9257f996b3d0febaa3e1fbbe230453
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-03-12 11:57:01 +0530

    Update LOOK_&_SAY.py

--- LOOK_&_SAY.py
+++ LOOK_&_SAY.py
@@ -1,7 +1,7 @@
 from itertools import groupby
 
 def lookandsay(s):
-	return ''.join(str(len(list(g)))+k for k,g in groupby(s))
+	return ''.join(str(len(list(g)))+k for k,g in groupby(s))#[(k,g),(k,g)]
 n='1'
 for i in range(5):
 	print(n)
